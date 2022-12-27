4c3c87b308d48c4787b272c2842cfa92cc7c8663
venustrine <82337821+venustrine@users.noreply.github.com>
2022-12-27 09:56:23 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version "cerulean"
+game "gta5"
+
+
+author "venus#8501" ##discord 
+https://github.com/venustrine/Polyzones
+
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@PolyZone/CircleZone.lua", "client/*.lua"}
