4a6177ae9c58657133c5dee661d80b1eeb707799
venustrine <82337821+venustrine@users.noreply.github.com>
2022-12-27 18:10:41 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,4 +4,4 @@ game "gta5"
 author "venus#8501" ##discord 
 description "https://github.com/venustrine/Polyzones"
 
-client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@PolyZone/CircleZone.lua", "@PolyZone/EntityZone.lua" "@PolyZone/CircleZone.lua"  "client/*.lua"}
+client_scripts {"@PolyZone/client.lua", "@PolyZone/BoxZone.lua", "@PolyZone/CircleZone.lua", "@PolyZone/EntityZone.lua", "@PolyZone/CircleZone.lua", "client/*.lua"}
